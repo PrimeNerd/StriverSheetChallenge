531e7dc2ba6957bcdfcd57d7b9a1782365af3b25
Aishwary Rudra Chaturvedi <83540171+PrimeNerd@users.noreply.github.com>
2022-06-14 07:53:32 +0530

    Add files via upload

---
+++ setmatrixzero.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+void setZeros(vector<vector<int>> &matrix)
+{
+	// Write your code here.
+    int n=matrix.size();
+    int m=matrix[0].size();
+    vector<int> a(n,-1);
+    vector<int> b(m,-1);
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(matrix[i][j]==0){
+                a[i]=0;
+                b[j]=0;
+            }
+        }
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(a[i]==0 || b[j]==0){
+                matrix[i][j]=0;
+            }
+        }
+    }
+}
+int main(){
+    //main code
+}
